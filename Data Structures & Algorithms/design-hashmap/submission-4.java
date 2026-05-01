ca94136a9ef483beed8ada1532d8919ff3c355c4
Jai Shree <157066574+sreemeenu15@users.noreply.github.com>
2026-05-01 22:47:59 +0530

    Add: design-hashmap - submission-4

---
+++ Data Structures & Algorithms/design-hashmap/submission-4.java
@@ -0,0 +1,37 @@
+class MyHashMap {
+
+    int n = 1000001;
+    int[] set = new int[n];
+    boolean[] set1 = new boolean[n];
+
+    public MyHashMap() {
+        
+    }
+    
+    public void put(int key, int value) {
+        
+        set[key] = value;
+        set1[key] = true;
+        
+    }
+    
+    public int get(int key) {
+        
+       if(set1[key])return set[key];
+       return -1;
+    }
+    
+    public void remove(int key) {
+        
+        
+        set1[key] = false;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
